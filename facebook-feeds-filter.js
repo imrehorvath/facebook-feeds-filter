26e714d9cd61380b1a2edace15f91c70cabd1d61
Imre Horvath <imi.horvath@gmail.com>
2023-01-26 17:24:51 +0100

    Create facebook-feeds-filter.js

---
+++ facebook-feeds-filter.js
@@ -0,0 +1,98 @@
+/// facebook-feeds-filter.js
+(function() {
+    const categoriesToHide = '{{1}}';  // Optional argument to filter posts by their category
+    const setOfCategoriesToHide = (( ) => {
+        if ( categoriesToHide === '' || categoriesToHide === '{{1}}' ) { return new Set(); }
+        return new Set(categoriesToHide.split(/\s*\|\s*/).map(s => s.toUpperCase()));
+    })();
+    const magic = String.fromCharCode(Date.now() % 26 + 97) +
+                  Math.floor(Math.random() * 982451653 + 982451653).toString(36);
+    const findNestedProperty = (obj, patt, fn, depth) => {
+        const helper = (objs, depth) => {
+            for ( const o of objs ) {
+                let keys = Object.keys(o).filter(k => k.match(patt));
+                if ( keys.length === 1 ) {  // We expect a single match
+                    return fn(o[keys[0]]);
+                }
+            }
+            if ( depth ) {
+                return helper(objs.flatMap(o => Object.values(o)
+                                                .filter(o => typeof o === 'object' && 
+                                                        o != null)),
+                              depth - 1);
+            }
+            return undefined;
+        };
+        return helper(new Array(obj), depth);
+    };
+    const processInsertedFeedUnit = (feedUnit, category) => {
+        switch ( category ) {
+            case 'ORGANIC':
+                // Organic feed units are the regular feed units and gets never filtered
+                break;
+            case 'SPONSORED':
+                // Sponsored feed units gets always filtered
+                feedUnit.classList.add(magic);
+                break;
+            default:
+                // Categories like ENGAGEMENT and PROMOTION gets filtered optionally.
+                if ( setOfCategoriesToHide.has(category) ) {
+                    feedUnit.classList.add(magic);
+                }
+        }
+    };
+    const checkWhetherFeedUnit = ( node ) => {
+        if ( node instanceof HTMLDivElement ) {
+            const keys = Object.keys(node).filter(k => k.startsWith('__reactProps'));
+            if ( keys.length === 1 ) {
+                const obj = node[keys[0]];
+                const root = obj.children &&
+                      obj.children.props &&
+                      obj.children.props.children &&
+                      obj.children.props.children.props;
+                if ( root ) {
+                    const category = findNestedProperty(root, /feed/, o => o.category, 4);
+                    if ( category ) {
+                        processInsertedFeedUnit(node, category);
+                    }
+                }
+            }
+        }
+    };
+    const start = ( ) => {
+        const style = document.createElement('style');
+        style.innerHTML = `
+            .${magic} {
+                display: none !important;
+            }
+        `;
+        document.head.appendChild(style);
+        const observer = new MutationObserver(mutations => {
+            mutations.forEach((mutation) => {
+                mutation.addedNodes.forEach((node) => {
+                    checkWhetherFeedUnit(node);
+                });
+            });
+        });
+        observer.observe(document.body, { childList: true, subtree: true });
+    };
+    window.addEventListener('load', (event) => {
+        document.addEventListener('click', (event) => {
+            const target = event.target.closest('a');
+            if ( target ) {
+                const url = new URL(target.href);
+                if ( url.searchParams.get('fbclid') ) {
+                    event.stopPropagation();
+                    event.preventDefault();
+                    url.searchParams.delete('fbclid');
+                    window.open(url.href, '_blank').focus();
+                }
+            }
+        });
+    });
+    if ( document.readyState === 'loading' ) {
+        document.addEventListener('DOMContentLoaded', start, { once: true });
+    } else {
+        start();
+    }
+})();
