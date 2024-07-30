f1c1f645b39ec6c03ee85fa251bde01956a80f15
Imre Horvath <imi.horvath@gmail.com>
2024-07-30 23:08:22 +0200

    Update facebook-feeds-filter.js

--- facebook-feeds-filter.js
+++ facebook-feeds-filter.js
@@ -52,13 +52,14 @@
                       obj.children.props.children.props.children &&
                       obj.children.props.children.props.children.props;
                 if ( root ) {
+                    console.log(`facebook-feeds-filter: root: ${root}`)
                     const category = findNestedProperty(root, 
                                                         /feed/, 
                                                         o => findNestedProperty(o, /category/, x => x, 0) || 
                                                             findNestedProperty(o, /story_cat/, x => x, 0),
                                                         4);
-                    console.log(`facebook-feeds-filter: category: ${category}`)
                     if ( category ) {
+                        console.log(`facebook-feeds-filter: category: ${category}`)
                         processInsertedFeedUnit(node, category);
                     }
                 }
